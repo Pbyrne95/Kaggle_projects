42e333fcce2804e5c103f43543a80afd728b770b
Philip Byrne <byrnepj@cardiff.ac.uk>
2021-02-02 14:02:30 +0000

    adding eveyrhitng else

--- leetcode/q_315.py
+++ leetcode/q_315.py
@@ -1,19 +1,44 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+## first try -> wrong 
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         self.maximum = float('-inf')
 
         def dfs(root):
-            if root is None:
+            if root == None:
                 return 0
             left_max = max(0,dfs(root.left))
             right_max = max(0,dfs(root.right))
-            self.maxium = max(self.maximum, left_max + right_max + root.val)
+            self.maximum = max(self.maximum, left_max + right_max + root.val)
 
             return max(left_max,right_max)+root.val
+        dfs(root)
         return self.maximum
+
+## second solution
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        if root in None:
+            return 0
+        self.s = 0
+        self.sum(root)
+        return self.s
+    def sum(self,root):
+        if root is None:
+            return 
+        if root.val%2 ==0:
+            if root.left:
+                if root.left.lef:
+                    self.s+=root.left.left.val
+                if root.left.right:
+                    self.s+=root.left.right.val
+            if root.right:
+                if root.right.left:
+                    self.s+=root.right.left.val
+                if root.right.right:
+                    self.s+=root.right.right.val
+        self.sum(root.left)
+        self.sum(root.right)
+
+
+
