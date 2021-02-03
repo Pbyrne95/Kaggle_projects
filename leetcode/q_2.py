40788988a39fdf13b47ae7084012b928568ae6d6
Philip Byrne <byrnepj@cardiff.ac.uk>
2021-02-03 10:27:17 +0000

    adding final solution 2

--- leetcode/q_2.py
+++ leetcode/q_2.py
@@ -3,7 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def addTwoNumbers(l1, l2):
+# stringifying the solution into a linked list 
+def addTwoNumbers1(l1, l2):
     joined_l1 = ''
     joined_l2 = ''
     for i in range(len(l1)):
@@ -20,4 +21,45 @@ def addTwoNumbers(l1, l2):
 
 l1 = [2,4,3]
 l2 = [5,6,4]
-print(addTwoNumbers(l1,l2))
+
+
+# Solution 2 -> using a linked list 
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        p1 = l1
+        p2 = l2
+
+
+        currentCarry = 0 
+
+        head = cur = ListNode(0)
+
+        while p1 or p2 or currentCarry:
+
+            currentVal = currentCarry
+            currentVal += 0 if p1 is None else p1.val
+            currentVal += 0 if p2 is None else p2.val
+            if currentVal >= 10:
+                currentVal -= 10
+                currentCarry = 1
+            else:
+                currentCarry = 0 
+            
+            cur.next = ListNode(currentVal)
+            cur = cur.next
+
+            if p1 is None and p2 is None:
+                break
+            elif p1 is None:
+                p2 = p2.next 
+            elif p2 is None:
+                p1 = p1.next
+            else:
+                p1 = p1.next 
+                p2 = p2.next
+        return head.next    
+
+
+
+
